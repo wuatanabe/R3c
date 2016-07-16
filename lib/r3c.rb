7d37942731d357ebcec18518b8a5dd537d7146ce
Paolo Freuli <paolo.freuli@gmail.com>
2016-07-16 22:37:35 +0200

    commented lines on lib/r3c.rb

--- lib/r3c.rb
+++ lib/r3c.rb
@@ -38,10 +38,10 @@ module R3c
 end
 
 
-R3c.site('http://localhost:3000/')
-R3c.format(:xml)
+# R3c.site('http://localhost:3000/')
+# R3c.format(:xml)
 
-R3c.auth({api: {key: '8091d55257c4c90b6d56e83322622cb5f4ecee64'}})
+# R3c.auth({api: {key: '8091d55257c4c90b6d56e83322622cb5f4ecee64'}})
 
 #~ file = File.read('c:\windows-version.txt')
 #~ puts file.inspect.to_s
